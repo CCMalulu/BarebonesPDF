51bc30752bc17e114687a1640c74694d507c521e
Troy Martin <beef.erikson.studios@gmail.com>
2019-08-25 23:30:43 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BarebonesPDF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Dock = System.Windows.Forms.DockStyle.Top;
+            this.buttonLoad.Location = new System.Drawing.Point(0, 0);
+            this.buttonLoad.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(1571, 23);
+            this.buttonLoad.TabIndex = 0;
+            this.buttonLoad.Text = "Load PDF";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            this.buttonLoad.Click += new System.EventHandler(this.buttonLoad_Click);
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(0, 23);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(1571, 1475);
+            this.axAcroPDF1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1571, 1498);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Controls.Add(this.buttonLoad);
+            this.Name = "Form1";
+            this.Text = "BarebonesPDF v1.0 - Beef Erikson Studios";
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoad;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BarebonesPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Opens a file dialog to open the PDF to be viewed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            // creates dialog
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            // sets filter and shows
+            fileDialog.Filter = "PDF files (*.pdf) |*.pdf;";
+            fileDialog.ShowDialog();
+
+            // load file and display if not null
+            if (fileDialog.FileName != null)
+                axAcroPDF1.LoadFile(fileDialog.FileName);
+        }
+    }
+}
